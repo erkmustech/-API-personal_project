c2873b19cf848847042426ce1098184e038358cf
erk.mustech <erk.mustech@gmail.com>
2021-06-30 08:48:58 +0200

    git ignore cancelled

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+export const SEARCH_BY_REGION_API =
+  "https://www.themealdb.com/api/json/v1/1/list.php?a=list";
+export const SEARCH_API =
+  "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+export const DEFAULT_URL_API = "https://www.themealdb.com/api/json/v1/1/";
+export const FILTER_BY_ARE='www.themealdb.com/api/json/v1/1/filter.php?a=';
