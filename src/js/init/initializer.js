18ce98f16ad4c216b24b969fd669bbb59792bed9
erk.mustech <erk.mustech@gmail.com>
2021-07-02 11:40:27 +0200

    update

--- src/js/init/initializer.js
+++ src/js/init/initializer.js
@@ -3,6 +3,7 @@ import searchRecipe from "../handler/searchRecipe.js";
 import populateRegion from "../handler/populateRegions.js";
 import renderMealsByArea from "../views/renderMealsByArea.js";
 import { COUNTRIES_LIST_API } from "../config.js";
+import renderMeals from "../views/renderMeals.js";
 
 function fetchAreaAPI(url) {
   fetch(COUNTRIES_LIST_API)
@@ -18,17 +19,30 @@ function fetchAreaAPI(url) {
 let searchTimeOutToken = 0;
 window.onload = () => {
   const searchField = document.querySelector(".search_field");
-  const resetBtn = document.querySelector(".reset_btn");
-  resetBtn.addEventListener("click", onload);
   fetchAreaAPI();
   searchField.onkeyup = () => {
     clearTimeout(searchTimeOutToken);
     if (searchField.value.trim().length === 0) {
       return;
     }
-    // searchRecipe(searchField.value);
     searchTimeOutToken = setTimeout(() => {
       searchRecipe(searchField.value);
-    }, 1000);
+    }, 500);
   };
+  const resetBtn = document.querySelector(".reset_btn");
+  resetBtn.addEventListener("click", onload);
 };
+
+// const main = async () => {
+//   const searchField = document.querySelector(".search_field");
+//   const resetBtn = document.querySelector(".reset_btn");
+//   fetchAreaAPI();
+//   if (searchField.value.trim().length === 0) {
+//     return;
+//   }
+//   resetBtn.addEventListener("click", () => {
+//     searchRecipe(searchField.value);
+//   });
+// };
+
+// window.addEventListener("load", main);
